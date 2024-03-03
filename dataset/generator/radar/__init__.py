f8145dff35a0dae3a6e65ef7d279cbe282a8100e
N3xTr1Me <destriker17@gmail.com>
2024-03-04 01:02:50 +0300

    Beam module moved to signals to prevent circular import

--- dataset/generator/radar/__init__.py
+++ dataset/generator/radar/__init__.py
@@ -1,7 +1,8 @@
-from .beams import BeamFactory, Beam
-
+from dataset.generator.signals.beams import BeamFactory, Beam
+from .radar import Radar
 
 __all__ = [
     "Beam",
-    "BeamFactory"
+    "BeamFactory",
+    "Radar"
 ]
